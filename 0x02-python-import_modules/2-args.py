f0d6b07550f65dd96d80d6ca92411751ced5fcbc
MISTOM <kigardetom2001@gmail.com>
2021-09-15 12:42:31 -0700

    accept passed arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 # 2-args.py
 
 if __name__ == "__main__":
-    """prints the arguments passed and lists them"""
+    """Print the number of and list of arguments."""
     import sys
 
     count = len(sys.argv) - 1
@@ -12,5 +12,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
-        for i in range(count):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
