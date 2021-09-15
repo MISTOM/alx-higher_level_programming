1e6fb336a32fe80d1cefa612377a13e37028566f
MISTOM <kigardetom2001@gmail.com>
2021-09-15 12:52:19 -0700

    add infinite numbers

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 3-infinite_add.py
+
+if __name__ == "__main__":
+    """Add infinite number of arguments"""
+    import sys
+    
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+
+    print("{}".format(total))
