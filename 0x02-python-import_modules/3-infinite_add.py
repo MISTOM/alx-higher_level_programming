5127ae825d1b912e64ead3096e555dae73c4a04f
MISTOM <kigardetom2001@gmail.com>
2021-09-15 12:54:15 -0700

    add infinite numbers

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,6 @@
 if __name__ == "__main__":
     """Add infinite number of arguments"""
     import sys
-    
     total = 0
     for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
