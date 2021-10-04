dc1bd1decfe3c236bd1a434bebe3a33f970cd5ba
KIGARDE TOM <75412361+MISTOM@users.noreply.github.com>
2021-10-04 23:08:10 +0300

    Update 1-rectangle.py
    add descrption

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
-
-'''Defining class Rectangle'''
+# 1-rectangle.py
+"""Defining class Rectangle
+"""
 
 class Rectangle:
-
-    '''Class rectangle with properties @width and @height'''
+    """Class rectangle with properties @width and @height
+    """
 
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle
+            
+        Args: 
+            width (int): Width of the rectangle
+            height (int): Height of the rectangle
+        """
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
+        """Get the width of the triangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set the width of the triangle"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -24,10 +33,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get the height of the triangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set the height of the triangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
