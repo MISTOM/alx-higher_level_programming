ced581b337e290560abb829977a632438c519046
MISTOM <kigardetom2001@gmail.com>
2021-10-18 11:50:29 -0700

    add class and method documentation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,12 +4,21 @@
 
 
 class Base:
-    """Base class implementation"""
+    """Base class implementation
+        
+        Attributes:
+        __nb_objects (int): The number of instanciated bases
+
+    """
     __nb_objects = 0
     def __init__(self, id=None):
+        """Initialize new base
+        Args:
+        id (int): Id's of the instanciated Base
+        """
         if not id == None:
             __nb_objects = id
 
         else:
-            __nb_objects = __nb_objects + 1
-            self.id = __nb_objects
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = Base.__nb_objects
