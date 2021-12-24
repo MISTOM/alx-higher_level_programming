2a603a26aa8eea3bcf99fe56c2dc5e820b85ead7
KIGARDE TOM <75412361+MISTOM@users.noreply.github.com>
2021-12-24 23:58:54 +0300

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
 """
-lists all states from the database hbtn_0e_0_usa
+Takes in an argument and displays all values in the states tables of a database
+where name matches the argument
 """
 
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
     """ not be executed when imported """
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db = sys.argv[3]
+    host = 'localhost'
+    port = 3306
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name = sys.argv[3]
+    state = str(sys.argv[4])
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=user,
-                           passwd=passwd, db=db, charset="utf8")
+    conn = MySQLdb.connect(host=host,
+                           port=port,
+                           user=username,
+                           passwd=password,
+                           db=name,
+                           charset="utf8"
+                           )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    # HERE I have to know SQL to grab all states in my database
+    sql = """SELECT * FROM states WHERE name LIKE BINARY '{}'
+          ORDER BY id ASC""".format(state)
+    cur.execute(sql)
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
