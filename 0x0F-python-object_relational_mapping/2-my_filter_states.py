b73d4d878f943d189a24e185dd0ea42a1ab3b898
KIGARDE TOM <75412361+MISTOM@users.noreply.github.com>
2021-12-24 23:49:54 +0300

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """
-List all states with a name starting with N (upper N)
+lists all states from the database hbtn_0e_0_usa
 """
+
+import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
     """ not be executed when imported """
-    import MySQLdb
-    from sys import argv
-    conn = MySQLdb.connect(host='localhost',
-                           port=3306,
-                           user=argv[1],
-                           passwd=argv[2],
-                           db=argv[3],
-                           charset="utf8")
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=user,
+                           passwd=passwd, db=db, charset="utf8")
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%'
-                ORDER BY states.id ASC""")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    # HERE I have to know SQL to grab all states in my database
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
